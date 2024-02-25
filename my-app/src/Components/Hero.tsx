166a7d0a33ae4f75eb21484e38baabab026277f6
julieluangpraxay <julieluangpraxay@gmail.com>
2024-02-25 19:13:04 +0000

    Moved bunny css to the top

--- my-app/src/Components/Hero.tsx
+++ my-app/src/Components/Hero.tsx
@@ -1,15 +1,6 @@
 export default function Hero() {
   return (
     <>
-      <div className="w-full flex flex-col items-center p-8">
-        <h1 className="text-5xl p-2">Julie Luangpraxay</h1>
-        <h3 className="text-xl p-2">Front-End Developer</h3>
-        <img
-          src="/images/Gif.gif"
-          alt=""
-          className="w-1/2 flex justify-center m-auto mb-20 rounded-3xl"
-        />
-      </div>
       <div className="box">
         <div className="head">
           <div className="ear-left">
@@ -29,8 +20,19 @@ export default function Hero() {
           </div>
           <div className="blush-left"></div>
           <div className="blush-right"></div>
-          <h1 className="flex flex-wrap relative ml-40">hello, welcome to my portfolio!</h1>
+          <h1 className="flex flex-wrap relative ml-40">
+            hello, welcome to my portfolio!
+          </h1>
         </div>
+      </div>
+      <div className="w-full flex flex-col items-center p-8">
+        <h1 className="text-5xl p-2">Julie Luangpraxay</h1>
+        <h3 className="text-xl p-2">Front-End Developer</h3>
+        <img
+          src="/images/Gif.gif"
+          alt=""
+          className="w-1/2 flex justify-center m-auto mb-20 rounded-3xl"
+        />
       </div>
     </>
   );
